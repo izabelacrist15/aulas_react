d0f76ab7d312d33138baf32957c89e3f14ea461b
Iza <beladaluz12@gmail.com>
2023-08-25 15:46:57 -0300

    segunda rota

--- 1_REACT_ROUTER_DOM/3_segunda_rota/src/router.jsx
+++ 1_REACT_ROUTER_DOM/3_segunda_rota/src/router.jsx
@@ -1,14 +1,10 @@
 import { Routes, Route} from "react-router-dom"
 import Home from "./pages/Home"
-import Usuarios from "./pages/Usuarios"
-
-
 function Router(){
     return(
-     <Routes>
+     <Route>
         <Route path="/" element={<Home/>}/>
-        <Route path="/usuarios" element={<Usuarios />}/>
-     </Routes>
+     </Route>
     )
 }
 
